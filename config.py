5087d215f23599de8c4e31270d1d4e169e95c400
mkvbui02-sudo <mkvbui02@gmail.com>
2025-09-14 09:37:00 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,4 +1,4 @@
-api_id = "24473318"
-api_hash = "e7dd0576c5ac0ff8f90971d6bb04c8f5"
-bot_token = "8034069514:AAHUBpzSCq41jPwsJkDbXuEoVC_yCxzyuw0"
-auth_users = [5840594311]
+api_id = "23557588"
+api_hash = "7ed52f38f74fe85ebc2e5e285051c3b6"
+bot_token = "8237804548:AAEKaoUNg-sEFGqu4FPvyAQzi5W52dYcu2k"
+auth_users = [2084001270]
